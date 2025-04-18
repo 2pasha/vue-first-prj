937aa91d0958f37ce4b14cb1cf191aba3c791faa
vatra <vatra@weareclario.com>
2025-04-18 16:15:06 +0300

    add base url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-first-prj',
   plugins: [
     vue(),
     vueDevTools(),
